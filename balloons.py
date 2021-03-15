6a02e51b35fb5873d501b5a040f062e0e27767b4
marsz <zacharymtrammell@mail.com>
2021-03-15 08:34:27 -0400

    basic image test in gazebo

---
+++ balloons.py
@@ -0,0 +1,12 @@
+import rospy
+from turtleAPT import robot
+import cv2
+
+try:
+    turtle = robot()
+    while not rospy.is_shutdown():
+        frame = turtle.getImage()
+        cv2.imshow("CAMERA",frame)
+        cv2.waitKey(27)
+except:
+    rospy.loginfo("Terminating")
