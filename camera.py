4d94fa7fc9ca0515edba85b10d7bd97c04501a50
marsz <zacharymtrammell@mail.com>
2021-03-15 09:42:03 -0400

    try changing pixels in red

--- camera.py
+++ camera.py
@@ -6,6 +6,11 @@ try:
     turtle = robot()
     while not rospy.is_shutdown():
         frame = turtle.getImage()
+        for pixel in frame:
+            if pixel[0] > pixel[1] and pixel[0] > pixel[2]:
+                pixel = (255,255,255)
+            else:
+                pixel = (0,0,0)
         cv2.imshow("CAMERA",frame)
         cv2.waitKey(27)
 except:
