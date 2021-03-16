7cb8c781518eab07262167071dfede4c017444c9
marsz <zacharymtrammell@mail.com>
2021-03-16 08:21:42 -0400

    type of image

--- camera.py
+++ camera.py
@@ -40,7 +40,7 @@ try:
             frame_threshed += red_thresh
 
         frame_threshed = cv2.bitwise_and(frame,frame,mask = frame_threshed)
-        print(type(frame_threshed))
+        print(type(frame))
         cv2.imshow("CAMERA",frame_threshed)
         cv2.waitKey(27)
 except Exception as e:
