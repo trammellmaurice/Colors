8c484520d1b21ea2d19c836a708ff9d852440138
marsz <zacharymtrammell@mail.com>
2021-03-15 21:25:20 -0400

    CAMERA THRESHOLD

--- camera.py
+++ camera.py
@@ -3,8 +3,9 @@ import numpy as np
 from turtleAPI import robot
 import cv2
 
-RED_MIN = np.array([100, 50, 50],np.uint8)
-RED_MAX = np.array([255, 50, 50],np.uint8)
+BLUE_MIN = np.array([90, 100, 20],np.uint8)
+BLUE_MAX = np.array([125, 255, 255],np.uint8)
+
 
 try:
     turtle = robot()
@@ -12,7 +13,7 @@ try:
         frame = turtle.getImage()
         hsv_img = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
-        frame_threshed = cv2.inRange(hsv_img, RED_MIN, RED_MAX)
+        frame_threshed = cv2.inRange(hsv_img, BLUE_MIN, BLUE_MAX)
         cv2.imshow("CAMERA",frame_threshed)
         cv2.waitKey(27)
 except:
