8be8d7159f614a85ee363443c15797ae750e9cd1
marsz <zacharymtrammell@mail.com>
2021-03-16 07:35:41 -0400

    test hough circle detector

--- camera.py
+++ camera.py
@@ -19,19 +19,34 @@ colors = {
 }
 
 
-
 try:
+    # INITIALIZE TURTLEBOT API
     turtle = robot()
+    # GET DESIRED COLOR
     desired_color = input("RED, GREEN, BLUE, OR PINK?")
     # print(colors[desired_color])
     while not rospy.is_shutdown():
+
+        # GET FRAME
         frame = turtle.getImage()
+        # CONVERT FRAME TO HSV
         hsv_img = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
+        # THRESHOLD USING DESIRED COLOR
         frame_threshed = cv2.inRange(hsv_img, colors[desired_color]["min"], colors[desired_color]["max"])
         if desired_color == "red":
+            # SECOND BAND FOR RED THRESHOLD
             red_thresh = cv2.inRange(hsv_img, np.array([170,50,50],np.uint8), np.array([180,255,255],np.uint8))
             frame_threshed += red_thresh
+
+        circles = cv2.HoughCircles(frame_threshed, cv2.HOUGH_GRADIENT, 1.2, 100)
+
+        if circles is not None:
+    	       circles = np.round(circles[0, :]).astype("int")
+               for (x,y,r) in circles:
+                   cv2.circle(frame_threshed, (x, y), r, (0, 255, 0), 4)
+                   cv2.rectangle(frame_threshed, (x - 5, y - 5), (x + 5, y + 5), (255, 0, 0), -1)
+
         cv2.imshow("CAMERA",frame_threshed)
         cv2.waitKey(27)
 except Exception as e:
