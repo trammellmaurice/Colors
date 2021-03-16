14df3747ed7820f14c6df913b742f2eb33ffed6d
marsz <zacharymtrammell@mail.com>
2021-03-15 22:15:43 -0400

    typo fix

--- camera.py
+++ camera.py
@@ -6,23 +6,23 @@ import cv2
 colors = {
     # BLUE VALUES
     "blue":
-    [BLUE_MIN = np.array([90, 100, 20],np.uint8),
-    BLUE_MAX = np.array([125, 255, 255],np.uint8)],
+    [np.array([90, 100, 20],np.uint8),
+    np.array([125, 255, 255],np.uint8)],
 
     # PINK VALUES
     "pink":
-    [PINK_MIN = np.array([135, 50, 20],np.uint8),
-    PINK_MAX = np.array([165, 255, 255],np.uint8)],
+    [np.array([135, 50, 20],np.uint8),
+    np.array([165, 255, 255],np.uint8)],
 
     # GREEN VALUES
     "green":
-    [GREEN_MIN = np.array([35, 100, 20],np.uint8),
-    GREEN_MAX = np.array([75, 255, 255],np.uint8)],
+    [np.array([35, 100, 20],np.uint8),
+    np.array([75, 255, 255],np.uint8)],
 
     # RED VALUES (0-10)
     "red":
-    [RED_MIN = np.array([0,50,50],np.uint8),
-    RED_MAX = np.array([10,255,255],np.uint8)],
+    [np.array([0,50,50],np.uint8),
+    np.array([10,255,255],np.uint8)],
 }
 
 
