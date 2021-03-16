7b6c480178b6444e5114ecf5a9dbff34c4a8303f
marsz <zacharymtrammell@mail.com>
2021-03-15 22:26:52 -0400

    dictionary format test

--- camera.py
+++ camera.py
@@ -34,7 +34,7 @@ try:
         frame = turtle.getImage()
         hsv_img = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
-        frame_threshed = cv2.inRange(hsv_img, np.array([90, 100, 20],np.uint8), np.array([125, 255, 255],np.uint8))
+        frame_threshed = cv2.inRange(hsv_img, desired_color["blue"]["min"], desired_color["blue"]["max"])
         if desired_color == "red":
             red_thresh = cv2.inRange(hsv_img, np.array([170,50,50],np.uint8), np.array([180,255,255],np.uint8))
             frame_threshed += red_thresh
