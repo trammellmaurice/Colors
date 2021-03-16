89d84de61f66bdec970b6cb2a0bb92c7d0896d88
marsz <zacharymtrammell@mail.com>
2021-03-16 10:41:26 -0400

    test turn

--- camera.py
+++ camera.py
@@ -61,12 +61,15 @@ try:
         # print(max_section)
         if max_section == "left2":
             print("LEFT")
+            turtle.drive(1,0)
         elif max_section == "left":
             print("LEFT")
-        if max_section == "right":
+            turtle.drive(1,0)
+        elif max_section == "right":
             print("RIGHT")
         elif max_section == "right2":
             print("RIGHT")
+
         frame_threshed = cv2.bitwise_and(frame,frame,mask = frame_threshed)
 
         # Blur using 3 * 3 kernel.
