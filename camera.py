787e3a3b46767e597794afca77920f11f2bb2bcf
marsz <zacharymtrammell@mail.com>
2021-03-15 21:54:49 -0400

    PINK THRESHOLD TEST

--- camera.py
+++ camera.py
@@ -6,6 +6,8 @@ import cv2
 BLUE_MIN = np.array([90, 100, 20],np.uint8)
 BLUE_MAX = np.array([125, 255, 255],np.uint8)
 
+PINK_MIN = np.array([135, 50, 20],np.uint8)
+PINK_MAX = np.array([165, 255, 255],np.uint8)
 
 try:
     turtle = robot()
@@ -13,7 +15,7 @@ try:
         frame = turtle.getImage()
         hsv_img = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
-        frame_threshed = cv2.inRange(hsv_img, BLUE_MIN, BLUE_MAX)
+        frame_threshed = cv2.inRange(hsv_img, PINK_MIN, PINK_MAX)
         cv2.imshow("CAMERA",frame_threshed)
         cv2.waitKey(27)
 except:
